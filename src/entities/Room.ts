b576133c60c6a5292622c9e871c17f6d746dce8d
Renan Augusto dos Santos <devrsantos@gmail.com>
2022-11-18 21:23:53 -0300

    Entities e Migrations

--- src/entities/Room.ts
+++ src/entities/Room.ts
@@ -1,5 +1,6 @@
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, ManyToMany, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import { Video } from './Video';
+import { Subject } from './Subject';
 
 @Entity('rooms')
 export class Room {
@@ -9,6 +10,12 @@ export class Room {
     @Column({ type: 'text' })
         name: string
 
+    @Column({ type: 'text', nullable: true })
+    description: string
+
     @OneToMany(() => Video, video => video.room)
     videos: Video[]
+
+    @ManyToMany(() => Subject, subject => subject.rooms)
+    subjects: Subject[]
 }
---
+++ src/entities/Subject.ts
@@ -0,0 +1,24 @@
+import { Column, Entity, JoinTable, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Room } from './Room';
+
+@Entity('subjects')
+export class Subject {
+    @PrimaryGeneratedColumn()
+        id: number
+
+    @Column({ type: 'text' })
+        name: string
+
+    @ManyToMany(() => Room, room => room.subjects)
+    @JoinTable({
+        name: 'room_subject',
+        joinColumn: {
+            name: 'room_id',
+            referencedColumnName: 'id'
+        },
+        inverseJoinColumn: {
+            name: 'subjects'
+        }
+    })
+    rooms: Room[]
+}
